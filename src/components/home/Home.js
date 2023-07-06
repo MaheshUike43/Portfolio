e043d1f73e5a543624e2e6d4d325b696bad86f35
MaheshUike43 <mah526840@gmail.com>
2023-07-06 20:45:41 +0530

    Portfolio Files Added

---
+++ src/components/home/Home.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../../../node_modules/bootstrap-icons/font/bootstrap-icons.min.css';
+import './home.css';
+import Homebg from '../../assets/Home-bg.jpg';
+
+export default function Home() {
+  const [textIndex, setTextIndex] = useState(0);
+  const dynamicTexts = ['MAHESH UIKE', 'REACT DEVELOPER', 'UI/UX DESIGNER'];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTextIndex((prevIndex) => (prevIndex + 1) % dynamicTexts.length);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <div className='black-bg'>
+        <div className='container'>
+          <div className="row text-center">
+            <div className='col-lg-7 mx-auto' id='home-content'>
+              <p id='text-1'>HELLO I'M</p>
+              <ul id='dynamic-text'>
+                <li>
+                  <span className={textIndex === 0 ? 'visible' : 'hidden'}>
+                    {dynamicTexts[0]}
+                  </span>
+                </li>
+                <li>
+                  <span className={textIndex === 1 ? 'visible' : 'hidden'}>
+                    {dynamicTexts[1]}
+                  </span>
+                </li>
+                <li>
+                  <span className={textIndex === 2 ? 'visible' : 'hidden'}>
+                    {dynamicTexts[2]}
+                  </span>
+                </li>
+              </ul>
+              <p id='text-2'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Consequuntur doloribus exercitationem a dolore non iusto similique
+                perferendis placeat consectetur!
+              </p>
+              <button className='' id='btn-cv'>Download CV</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <img className='img-fluid' id='home-bg' src={Homebg} alt="..." />
+    </div>
+  );
+}
---
+++ src/components/menu/Menu.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../../../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import '../../../node_modules/bootstrap-icons/font/bootstrap-icons.min.css'
+import './menu.css'
+import logo from '../../assets/Mahesh_logo1.png'
+import Home from '../home/Home'
+import About from '../about/About'
+import Services from '../services/Services'
+import Portfolio from '../portfolio/Portfolio'
+import Contact from '../contact/Contact'
+
+export default function Menu() {
+
+  const [toggle, setToggle] = useState(false);
+
+  const handleClick = () => {
+    setToggle(!toggle)
+  }
+
+  return (
+    <div>
+      <div className='container-fluid' id='menu-bar'>
+        <div className='container'>
+          <div className="row">
+            <div className="col-lg-3 col-md-12">
+              <img src={logo} alt="..." id='logo' className='navbar-brand' />
+              <i className="bi bi-list" onClick={handleClick} id='menu-icon'></i>
+            </div>
+            {toggle ?
+              <div className="col-lg-9 col-md-12" id='menu-list'>
+                <a className="nav-link" href='#home'>Home</a>
+                <a className="nav-link" href='#about'>About</a>
+                <a className="nav-link" href='#services'>Services</a>
+                <a className="nav-link" href='#portfolio'>Portfolio</a>
+                <a className="nav-link" href='#contact'>Contact</a>
+              </div>
+              :
+              <div className="col-lg-9 col-md-12" id='menu-list2'>
+                <a className="nav-link" href='#home'>Home</a>
+                <a className="nav-link" href='#about'>About</a>
+                <a className="nav-link" href='#services'>Services</a>
+                <a className="nav-link" href='#portfolio'>Portfolio</a>
+                <a className="nav-link" href='#contact'>Contact</a>
+              </div>
+            }
+          </div>
+        </div>
+      </div>
+      <div id='home'>
+        <Home />
+      </div>
+      <div id='about'>
+        <About />
+      </div>
+      <div id='services'>
+        <Services />
+      </div>
+      <div id='portfolio'>
+        <Portfolio />
+      </div>
+      <div id='contact'>
+        <Contact />
+      </div>
+    </div>
+  )
+}
